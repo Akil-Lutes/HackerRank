1bd60270a7b95a6c4cbb239a2494ca87cb0af14f
Akil Lutes <lutes.akil@gmail.com>
2021-12-10 00:26:31 -0600

    relearning miniMaxSum

--- miniMaxSum/miniMaxSum.js
+++ miniMaxSum/miniMaxSum.js
@@ -1,50 +1,73 @@
 /*
  * Complete the 'miniMaxSum' function below.
- *
+ * https://www.hackerrank.com/challenges/mini-max-sum/problem
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
 
-const array = [1, 2, 3, 4, 5];
-    // Write your code here
-    // find the max value in the array
-    // find the min value in the array
-    // find a way to add the values in the array and leave out the max or min
-    // I am going to need a for loop to add all of the values in the array.
-    // I might need a for loop to add all values in array.
-    // I will select the first and last values out of the array once I am done sorting
-    // I sliced the array from the first element
-    // I will have to slice the element from the last element and tie to a variable
-    // I am going to have to copy the array
-  
+
+// *** 12/8/2021 ***
+
+
 function miniMaxSum(arr) {
-    // my placeholder sums of min & max
-    let miniSum = 0;
-    let maxSum = 0;
-    // My copy of arrarys. I will be able to mutate the arrays w/o overwriting
-    let highCopy = [...arr];
-    let lowCopy = [...arr];
-    console.log(lowCopy);
-    console.log(highCopy);
-    // Sorting the arrays from high to low/low to high
-    let sortedHighArr = highCopy.sort(function(a, b) {
-        return a - b
-    })
-    let sortedLowArr = lowCopy.sort(function(a, b) {
-        return b - a;
-    })
-    // Slicing/pop the arrays from the 1st & last elements
-    let slicedHighArr = sortedHighArr.slice(1);
-    let slicedLowArr = sortedLowArr.slice(1);
+    // Write your code here
+    let minSumArr = arr.sort(function (a, b) { return b - a })
+    let maxSumArr = arr.sort(function (a, b) { return a - b });
+    
 
-    for (let i = 0; i < slicedHighArr.length && slicedLowArr.length; i++) {
-        miniSum += slicedHighArr[i];
-        maxSum += slicedLowArr[i];
-    }
-
-    return [maxSum, miniSum];
 }
 
-console.log(miniMaxSum(array));
+
+miniMaxSum([1, 3, 5, 7, 9]);
+
+
+
+
+
+
+
+
+
+// const array = [1, 2, 3, 4, 5];
+//     // Write your code here
+//     // find the max value in the array
+//     // find the min value in the array
+//     // find a way to add the values in the array and leave out the max or min
+//     // I am going to need a for loop to add all of the values in the array.
+//     // I might need a for loop to add all values in array.
+//     // I will select the first and last values out of the array once I am done sorting
+//     // I sliced the array from the first element
+//     // I will have to slice the element from the last element and tie to a variable
+//     // I am going to have to copy the array
+  
+// function miniMaxSum(arr) {
+//     // my placeholder sums of min & max
+//     let miniSum = 0;
+//     let maxSum = 0;
+//     // My copy of arrarys. I will be able to mutate the arrays w/o overwriting
+//     let highCopy = [...arr];
+//     let lowCopy = [...arr];
+//     console.log(lowCopy);
+//     console.log(highCopy);
+//     // Sorting the arrays from high to low/low to high
+//     let sortedHighArr = highCopy.sort(function(a, b) {
+//         return a - b
+//     })
+//     let sortedLowArr = lowCopy.sort(function(a, b) {
+//         return b - a;
+//     })
+//     // Slicing/pop the arrays from the 1st & last elements
+//     let slicedHighArr = sortedHighArr.slice(1);
+//     let slicedLowArr = sortedLowArr.slice(1);
+
+//     for (let i = 0; i < slicedHighArr.length && slicedLowArr.length; i++) {
+//         miniSum += slicedHighArr[i];
+//         maxSum += slicedLowArr[i];
+//     }
+
+//     return [maxSum, miniSum];
+// }
+
+// console.log(miniMaxSum(array));
 
 
 
